72932fd9e604e384fea4ab3f7c160161ae58d7dc
cherqim <mustapha.c.b@gmail.com>
2024-03-21 14:24:42 +0000

    Updated SVG type declaration

--- declarations.d.ts
+++ declarations.d.ts
@@ -6,7 +6,12 @@ declare module '*.webp' {
   const value: string
   export = value
 }
+
 declare module '*.jpeg' {
+  const value: string
+  export = value
+}
+declare module '*.svg' {
   const value: string
   export = value
 }
