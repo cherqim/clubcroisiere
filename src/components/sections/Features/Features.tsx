4535b8034c9acab7ccd6ad1c2629926bd8820154
cherqim <mustapha.c.b@gmail.com>
2024-03-22 10:59:31 +0000

    Accessibility Improvements Features, Banner

--- src/components/sections/Features/Features.tsx
+++ src/components/sections/Features/Features.tsx
@@ -1,65 +1,46 @@
-import React from 'react'
-import confettis from 'assets/confettis.png'
-import puzzle from 'assets/puzzle.webp'
-import boussole from 'assets/boussole.png'
+import bateau from 'assets/bateau.webp'
 import { motion } from 'framer-motion'
 
-const feats = [
-  {
-    title: 'Expérience unique',
-    desc: "Découvrez nos croisières conçues avec plus de 38 ans d'expérience, garantissant des voyages sans pareil.",
-    img: boussole
-  },
-  {
-    title: 'Événements Inoubliables',
-    desc: 'Explorez nos croisières clés en main, créant des moments inoubliables et des souvenirs pour toute une vie.',
-    img: confettis
-  },
-  {
-    title: 'Personnalisation Ultime',
-    desc: 'Profitez de notre personnalisation complète pour des voyages qui répondent à vos rêves et désirs les plus profonds.',
-    img: puzzle
-  }
-]
-
-const Features = () => {
-  const delayOrder = [2, 0, 1]
+const Banner = () => {
   return (
-    <section className="relative z-[9999] flex h-auto w-full flex-col items-center justify-center gap-5 bg-transparent md:mt-[-80px] md:flex-row">
-      {feats.map((feat, i) => (
-        <FeatureCard key={i} {...feat} delay={delayOrder[i] * 0.2} />
-      ))}
-    </section>
+    <header className="relative inset-0 mt-20 flex  w-full max-w-7xl flex-col items-center justify-center md:mx-20 md:h-[450px] md:flex-row">
+      <div className="bg-mapp absolute inset-0 -z-10"></div>
+      <motion.div
+        initial={{ opacity: 0, translateX: -200 }}
+        animate={{ opacity: 1, translateX: 0 }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        className="flex flex-col items-start justify-center gap-5 p-10 md:w-1/2"
+      >
+        <h2 className="font-heading text-[28px] font-semibold leading-none text-secondary md:text-[48px]">
+          Pourquoi Choisir Club Croisière ?
+        </h2>
+        <h4 className="font-p text-[14px] leading-[24px] text-accent md:text-[16px]">
+          Aventures inoubliables, conçus avec soin
+        </h4>
+        <p className="text-[14px] text-primary md:text-[16px]">
+          Fort de plus de 38 ans d’expérience, Club Croisière excelle dans
+          l’organisation de croisières et d’événements maritimes clés en main.
+          Chaque voyage est une promesse d’expériences inoubliables, conçu avec
+          soin pour satisfaire tous vos désirs.
+        </p>
+        <button className="btn-primary" aria-label="Reserve your spot now">
+          Réservez votre place maintenant
+        </button>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, translateX: 200 }}
+        animate={{ opacity: 1, translateX: 0 }}
+        transition={{ duration: 1, ease: 'easeInOu' }}
+        className="flex flex-col items-center justify-center md:w-1/2"
+      >
+        <img
+          src={bateau}
+          alt="A boat on the sea"
+          className="w-screen align-middle md:max-w-[51vw]"
+        />
+      </motion.div>
+    </header>
   )
 }
 
-const FeatureCard = ({
-  title,
-  desc,
-  img,
-  delay
-}: {
-  title: string
-  desc: string
-  img: string
-  delay: number
-}) => {
-  return (
-    <motion.div
-      initial={{ y: 300, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, delay: delay }}
-      className="m-3 flex h-80 w-96 flex-col items-center justify-center bg-white p-[48px] shadow-[0_-10px_30px_0_rgba(0,0,0,.1)]"
-    >
-      <img src={img} alt="boussole" className="mb-[15px] size-20" />
-      <h3 className="my-2 font-heading text-[18px] font-semibold text-secondary md:text-[24px]">
-        {title}
-      </h3>
-      <p className="h-28 text-center font-p text-[14px] font-normal leading-[24px] tracking-normal md:text-[16px]">
-        {desc}
-      </p>
-    </motion.div>
-  )
-}
-
-export default Features
+export default Banner
